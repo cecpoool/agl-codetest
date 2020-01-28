03422f2165b78cb99eea0abdd1ae9c8747a75db6
CECILIA COOK <101938034@student.swin.edu.au>
2020-01-28 15:37:11 +1100

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+class Index extends React.Component {
+  async componentDidMount() {
+    var femFiltered = [];
+    var maleFiltered = [];
+
+    let result = fetch(
+      "http://agl-developer-test.azurewebsites.net/people.json"
+    )
+      .then(response => response.json())
+      .then(data => console.log(data));
+
+    console.log(result);
+
+    for (var i = 0; i < result.length; i++) {
+      if (result[i].gender === "Female") {
+        femFiltered.push(result[i]);
+      } else {
+        maleFiltered.push(result[i]);
+      }
+    }
+    console.log(femFiltered);
+    console.log(maleFiltered);
+  }
+
+  render() {
+    return <h1>give it a try</h1>;
+  }
+}
+export default Index;
