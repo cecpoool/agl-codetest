cadd06c3160e48abc6893e1e91261986d57f8574
CECILIA COOK <101938034@student.swin.edu.au>
2020-01-29 12:13:30 +1100

    working on nested objects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-var femFiltered = [];
-var maleFiltered = [];
 
 class Index extends React.Component {
   constructor() {
@@ -19,23 +17,33 @@ class Index extends React.Component {
     var femFiltered = this.state.result.filter(function(e) {
       return e.gender === "Female";
     });
-    console.log(femFiltered);
     var maleFiltered = this.state.result.filter(function(e) {
       return e.gender === "Male";
     });
     console.log(femFiltered);
     console.log(maleFiltered);
 
-    // for (var i = 0; i < this.state.result.length; i++) {
-    //   if (this.state.result.gender === "Female") {
-    //     femFiltered.push(this.state.result[i]);
-    //   } else {
-    //     maleFiltered.push(this.state.result[i]);
-    //   }
-    // }
-    // console.log(femFiltered);
-    // console.log(maleFiltered);
-    return <h1>give it a try</h1>;
+    let pets = [];
+
+    try {
+      for (let man of maleFiltered) {
+        for (let pet of man.pets) {
+          pets.push(pet);
+        }
+      }
+      console.log(pets);
+    } catch (err) {
+      alert("error with new list");
+    }
+
+    return (
+      <div>
+        <h1>Fella's Cats</h1>
+        <ul></ul>
+        <h1>Gal's Cats</h1>
+        <ul></ul>
+      </div>
+    );
   }
 }
 export default Index;
